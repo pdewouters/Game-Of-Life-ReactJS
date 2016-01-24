2b8abcf72e559ede8214c19ee51801d59d34fa5a
Paul de Wouters <paul@hmn.md>
2016-01-24 13:33:23 +0000

    code is functional

--- src/App.js
+++ src/App.js
@@ -1,41 +1,112 @@
-import React, { Component } from 'react';
-import Cell from './Cell';
+import React, { Component } from 'react'
+import Cell from './Cell'
+import _ from 'underscore'
 
 export default class App extends Component {
     constructor(props){
         super(props)
         this.state = {
-            rows:4, cols:4, cells:[], generations:0
+            rowCount:50, colCount:70, rows:[], generations:0, delay: 2000
         }
     }
+
     componentDidMount(){
         this.initGrid()
+        this.timer = setInterval(() => {
+            this.step()
+        }, this.state.delay)
+       // this.step()
     }
+
+    componentWillUnmount(){
+        clearInterval(this.timer)
+    }
+
     defaultSeed(){
-        let seed = [{rowIdx:0,colIdx:0},{rowIdx:1,colIdx:1},{rowIdx:2,colIdx:2}]
-        for(var i=0;i<seed.length;i++){
-            this.toggleCell(seed[i])
-        }
-    }
-    toggleCell(coords){
-        // expects an object like {rowIdx:0,colIdx:0}
-        //this.state.cells[coords.rowIdx,coords.colIdx].status = !this.state.cells[coords.rowIdx,coords.colIdx].status
-    }
-    initGrid(){
-        for(var i=0;i<this.state.rows;i++){
-            for(var j=0;j<this.state.cols;j++){
-                this.state.cells.push(<Cell key={'cell-' + i.toString() + '-' + j.toString()} status={'dead'} coords={[i,j]} />)
+        var rowIdx = this.state.rowCount / 2, colIdx = this.state.colCount / 2
+        for(var i=rowIdx-5;i<=rowIdx+5;i++){
+            for(var j=colIdx-5;j<=colIdx+5;j++){
+                if(j%2===0){
+                    this.state.rows[i][j] = 'live'
+                }
             }
         }
-        this.defaultSeed()
-        let generations = this.state.generations +1
-        this.setState({cells:this.state.cells,generations:generations})
     }
+
+    toggleCell(){
+        cell.status = cell.status === 'live' ? 'dead' : 'live'
+    }
+
+    initGrid(){
+        for(var i=0;i<this.state.rowCount;i++){
+            this.state.rows[i] = []
+            for(var j=0;j<this.state.colCount;j++){
+                this.state.rows[i][j] = 'dead'
+            }
+        }
+        this.setState({rows:this.state.rows})
+        this.defaultSeed()
+        let generations = this.state.generations + 1
+        this.setState({rows:this.state.rows,generations:generations})
+    }
+
+    step(){
+        // Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+        // Any live cell with two or three live neighbours lives on to the next generation.
+        // Any live cell with more than three live neighbours dies, as if by over-population.
+        // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+        this.state.rows.forEach((row, rowIdx)=>{
+            row.forEach((cell, cellIdx)=> {
+
+                let live = 0, dead = 0
+                for(var i=rowIdx-1;i<=rowIdx+1;i++){
+                    for(var j=cellIdx-1;j<=cellIdx+1;j++){
+                        if(i<0 || i>this.state.rowCount-1){
+                            continue
+                        }
+                        if(j<0 || j>this.state.colCount-1){
+                            continue
+                        }
+                        if(i === rowIdx && j === cellIdx) {
+                            continue
+                        }
+                        if(this.state.rows[i][j]==='live'){
+                            live++
+                        }
+                        if(this.state.rows[i][j]==='dead'){
+                            dead++
+                        }
+                    }
+                }
+                if(this.state.rows[rowIdx][cellIdx] === 'live' && live < 2){
+                    this.state.rows[rowIdx][cellIdx] = 'dead'
+                } else if(this.state.rows[rowIdx][cellIdx] === 'live' && (live===2 || live === 3)) {
+                    this.state.rows[rowIdx][cellIdx] = 'live'
+                } else if(this.state.rows[rowIdx][cellIdx] === 'live' && live > 3) {
+                    this.state.rows[rowIdx][cellIdx] = 'dead'
+                } else if( cell === 'dead' && live === 3){
+                    this.state.rows[rowIdx][cellIdx] = 'live'
+                }
+
+            })
+        })
+
+        this.setState({rows:this.state.rows,generations: this.state.generations + 1})
+    }
+
     render(){
+        let cells = []
+        this.state.rows.forEach((row, rowIdx)=>{
+            row.forEach((cell, cellIdx)=> {
+                cells.push(<div onClick={this.toggleCell} key={'cell-' + rowIdx + '-' + cellIdx} className={'gol-cell ' + cell}></div>)
+            })
+        })
+
+        let width = this.state.colCount * 10, height = this.state.rowCount * 10
         return(
-            <div className="gol-app default">
+            <div className="gol-app default" style={{width: width + 'px', height: height + 'px'}}>
                 <h1>Game of life</h1>
-                {this.state.cells}
+                {cells}
             </div>
         )
     }
--- src/Cell.js
+++ src/Cell.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Cell extends Component {
     render(){
         return (
-            <div className="gol-cell"></div>
+            <div className={"gol-cell " + this.props.cell.status}></div>
         )
     }
 }
